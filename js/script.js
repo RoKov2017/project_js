6201206b8168743dc0cbb36bc5e776968c3e12f4
Roman <ancora.roman@gmail.com>
2023-05-17 20:02:51 +0300

    Форма. Fetch c помощью formData

--- js/script.js
+++ js/script.js
@@ -259,33 +259,36 @@ window.addEventListener('DOMContentLoaded', () => {
 				`;
 				form.insertAdjacentElement('afterend', statusMassege)
 
-				const request = new XMLHttpRequest();
-				request.open('POST', 'server.php');
+				// const request = new XMLHttpRequest();
+				// request.open('POST', 'server.php');
 
-				request.setRequestHeader('Content-type', 'application/json');
+				// request.setRequestHeader('Content-type', 'application/json');
 				const formData = new FormData(form);
 
-				const object = {};
-				formData.forEach(function(value, key) {
-					object[key] = value;
-				});
+				// const object = {};
+				// formData.forEach(function(value, key) {
+				// 	object[key] = value;
+				// });
 
-				const json = JSON.stringify(object);
+				// const json = JSON.stringify(object);
 
-				request.send(json);
 
-				request.addEventListener('load', () => {
-					if (request.status === 200) {
-						console.log(request.response);
+
+				fetch('server.php', {
+					method: "POST",
+					// headers: {
+					// 	'Content-type': 'application/json'
+					// },
+					body: formData
+				}).then(data => data.text())
+				.then(data => {
+						console.log(data);
 						showThanksModal(message.success);
-
-						form.reset();			//чистим форму
-
 						statusMassege.remove();
-
-					} else {
-						showThanksModal(message.failure);
-					}
+				}).catch(() => {
+					showThanksModal(message.failure);
+				}).finally(() => {
+					form.reset();			//чистим форму
 				});
 				 			
 			});
